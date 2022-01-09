d81b9f265056f130df0c6761d780d9c781053c7f
NirGrossman1986 <82283999+NirGrossman1986@users.noreply.github.com>
2022-01-09 12:42:44 +0200

    Update myApp-installation.sh

--- script/myApp-installation.sh
+++ script/myApp-installation.sh
@@ -7,4 +7,8 @@ aws configure set aws_secret_access_key <%=node["consul-mysql-npm"]["attr1"]["se
 aws configure set default.region eu-west-3
 
 aws s3 cp s3://workshop-tf-state-nirg/ChuckNorrisAPP/  /home/ubuntu/ChuckNorrisAPP --recursive
+
+cd /home/ubuntu/ChuckNorrisAPP
+
+sudo flask run --host=0.0.0.0 --port=5000
        
